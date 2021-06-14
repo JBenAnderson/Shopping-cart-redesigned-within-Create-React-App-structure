ffd57e24a626d03b0306ded58023ec0055737c1f
J Ben Anderson <revovisions@gmail.com>
2021-06-14 15:07:07 -0400

    created products.jsx within components folder, added necc. imports

---
+++ shopping-cart/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Products from "./Components/products";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return <Products></Products>;
+}
+
+export default App;
